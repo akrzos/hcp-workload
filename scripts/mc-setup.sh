00a17289d74deb043f6f1b5142ba3d71bb0f0d6d
Alex Krzos <akrzos@redhat.com>
2025-05-02 10:58:02 -0400

    Small patch to the mc-setup script

--- scripts/mc-setup.sh
+++ scripts/mc-setup.sh
@@ -3,6 +3,9 @@
 set -e
 set -o pipefail
 
+# Add toleration to the localvolume
+oc patch localvolume -n openshift-local-storage lv-worker-localstorage --type json -p '[{"op": "add", "path": "/spec/tolerations", "value": [{"key": "node-role.kubernetes.io/hcp", "value": "reserved", "effect": "NoExecute"}]}]'
+
 # Label the Nodes for HCP
 oc --kubeconfig /root/vmno/kubeconfig label no vm00006 node-role.kubernetes.io/hcp=
 oc --kubeconfig /root/vmno/kubeconfig label no vm00007 node-role.kubernetes.io/hcp=
@@ -17,9 +20,6 @@ oc --kubeconfig /root/vmno/kubeconfig label no vm00008 node-role.kubernetes.io/w
 oc --kubeconfig /root/vmno/kubeconfig adm taint nodes vm00006 node-role.kubernetes.io/hcp=reserved:NoExecute
 oc --kubeconfig /root/vmno/kubeconfig adm taint nodes vm00007 node-role.kubernetes.io/hcp=reserved:NoExecute
 oc --kubeconfig /root/vmno/kubeconfig adm taint nodes vm00008 node-role.kubernetes.io/hcp=reserved:NoExecute
-
-# Add toleration to the localvolume
-oc patch localvolume -n openshift-local-storage lv-worker-localstorage --type json -p '[{"op": "add", "path": "/spec/tolerations", "value": [{"key": "node-role.kubernetes.io/hcp", "value": "reserved", "effect": "NoExecute"}]}]'
 
 # Set PV for OpenShift-Monitoring
 oc --kubeconfig /root/vmno/kubeconfig apply -f mc-manifests/cluster-monitoring-config.yaml
