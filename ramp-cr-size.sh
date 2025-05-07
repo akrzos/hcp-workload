90bc98b25ef353ca0beef43e6054060891bbf15d
Alex Krzos <akrzos@redhat.com>
2025-05-07 16:58:32 -0400

    Initial Ramp CR Size test script

---
+++ ramp-cr-size.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# Ramp size of the CRs
+set -e
+set -o pipefail
+
+# Start Timestamp
+ts="$(date -u +%Y%m%d-%H%M%S)"
+
+# Workload Job Config
+export ITERATIONS=10
+export PROM_URL=https://$(oc -n openshift-monitoring get route prometheus-k8s -oyaml | grep host: | head -1 | awk '{ print $2 }')
+export PROM_TOKEN=$(oc -n openshift-monitoring create token prometheus-k8s)
+if [ -z ${ES_SERVER} ]; then export ES_SERVER=""; fi
+if [ -z ${ES_INDEX} ]; then export ES_INDEX=""; fi
+export LOCAL_INDEXING=true
+export JOB_PAUSE_TIME="3m"
+export QPS=40
+export BURST=80
+
+# Objects Config
+export CRDS=50
+export CRS=10
+# CR Size ramped in loop
+# export CR_SIZE=1024
+export SERVER_DEPLOYMENTS=2
+export CLIENT_DEPLOYMENTS=2
+export CONFIGMAPS=5
+export CM_KEY_COUNT=10
+export CM_VALUE_SIZE=1024
+export SECRETS=5
+export SECRET_KEY_COUNT=10
+export SECRET_VALUE_SIZE=1024
+export SERVICES=2
+export POD_COUNT=4
+export CONTAINER_COUNT=2
+export LABEL_COUNT=5
+export ENV_ADD_VAR_COUNT=5
+# Env var size in bytes
+# 1024 - 1KiB (1024 Bytes), 102400 - 100KiB Works
+# 128KiB (131072), 256KiB (262144), 512KiB (524288), 1MiB(1048576), All Too Large
+export ENV_ADD_VAR_SIZE=1024
+# export ENV_ADD_VAR_SIZE=1024
+# export ENV_ADD_VAR_SIZE=102400
+# export ENV_ADD_VAR_SIZE=1048576
+
+# Range of CR Sizes
+cr_size=("1024" "131072" "262144" "524288")
+
+cd results/
+for i in "${!cr_size[@]}"; do
+  export CR_SIZE=${cr_size[$i]}
+  echo "Running Test: $i, CRDs: ${CRDS}, CRs: ${CRS}, ${CR_SIZE}"
+  export METRICS_DIRECTORY="${ts}-ramp-cr-size-${i}-${CR_SIZE}"
+  log_file="${METRICS_DIRECTORY}.log"
+  time kube-burner init -c ../hcp-workload/job-workload.yml | tee ${log_file}
+  # time kube-burner init -c ../hcp-workload/job-workload.yml --log-level debug | tee ${log_file}
+done
+cd ..
