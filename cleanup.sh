eb5457fd90ba8a0f235fc84e88ba69bb6146811f
Alex Krzos <akrzos@redhat.com>
2025-05-15 09:18:25 -0400

    Add metrics-profile directory

--- cleanup.sh
+++ cleanup.sh
@@ -3,4 +3,4 @@
 set -e
 set -o pipefail
 
-time kube-burner-ocp init -c hcp-workload/job-cleanup.yml --check-health=false
+time kube-burner-ocp --check-health=false init -c hcp-workload/job-cleanup.yml
